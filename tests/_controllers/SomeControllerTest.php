0b770cddbea139f2b746cae63ec15968873fe003
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2013-06-12 13:00:59 -0300

    issue #8 build pipeline adding tests, coverage and other build reports

---
+++ tests/_controllers/SomeControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @group Controller
+ */
+
+class SomeControllerTest extends CIUnit_TestCase
+{
+	public function setUp()
+	{
+		// Set the tested controller
+		//$this->CI = set_controller('index');
+	}
+	
+	public function testIndexController()
+	{
+		// Call the controllers method
+		//$this->CI->index();
+		
+		// Fetch the buffered output
+		//$out = output();
+		
+		// Check if the content is OK
+		//$this->assertSame(0, preg_match('/(error|notice)/i', $out));
+	}
+}
