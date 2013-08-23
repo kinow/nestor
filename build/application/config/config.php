b65924d872ae76f3cba7615bc31261a3639a5f63
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2013-08-22 22:21:18 -0300

    DB config for demo app

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://nestor-qa.org/';
+$config['base_url']	= 'http://nestordemo.net76.net/';
 
 /*
 |--------------------------------------------------------------------------
