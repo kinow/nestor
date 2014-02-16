cbcda1169b12a7553411043fb1ba37f323c40c3e
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-02-16 12:43:04 -0300

    Issue #31 deploy Nestor demo

---
+++ Envoy.blade.php
@@ -0,0 +1,14 @@
+@servers(['web' => 'root@66.175.222.55'])
+
+@task('deploy', ['on' => 'web'])
+    cd /var/www/nestor/
+    git reset --hard HEAD
+    git clean -f
+    git pull origin master
+    composer dump-autoload
+    composer update --no-dev --prefer-dist --profile -vvv
+    rm -f app/database/production.sqlite
+    touch app/database/production.sqlite
+    php artisan migrate:install
+    php artisan migrate:refresh --seed
+@endtask
