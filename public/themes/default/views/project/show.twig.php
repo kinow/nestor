d10391956bb9cb49876a874d93bf396f2f51dd8f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-12-31 10:28:23 -0200

    Fix missing white space

--- public/themes/default/views/project/show.twig.php
+++ public/themes/default/views/project/show.twig.php
@@ -15,6 +15,7 @@
 		{{ Form.textarea('description', project.description, {'id': "description", 'rows': "3", 'class': "form-control", 'readonly': 'readonly'}) }}
     </div>
 </div>
+<br/>
 <div class='row'>
 	<div class='col-xs-offset-2 col-xs-8'>
 		{{ HTML.linkRoute('projects.edit', 'Edit', [project.id], {'class': 'btn btn-primary'}) }}
