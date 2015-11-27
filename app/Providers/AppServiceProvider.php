e9e2ecc74f3589ae016f4351d26187c8a4d79d5b
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-11-28 00:51:39 +1300

    Commenting the service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,22 +8,19 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-      app('Dingo\Api\Auth\Auth')->extend('basic', function ($app) {
-       return new \Dingo\Api\Auth\Provider\Basic($app['auth'], 'email');
-      });
+        app('Dingo\Api\Auth\Auth')->extend('basic', function ($app) {
+            return new \Dingo\Api\Auth\Provider\Basic($app['auth'], 'email');
+        });
 
-      $this->app->bind('Nestor\Repositories\UserRepository', 'Nestor\Repositories\UserRepositoryEloquent');
+        // bind repositories
+        $this->app->bind('Nestor\Repositories\UserRepository', 'Nestor\Repositories\UserRepositoryEloquent');
     }
 
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
