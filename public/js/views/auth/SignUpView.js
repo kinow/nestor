6aad7d296c638fafcff0e790ec589533748bc0a3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-12-06 22:59:10 +1300

    Preparing register screen and API integration

--- public/js/views/auth/SignUpView.js
+++ public/js/views/auth/SignUpView.js
@@ -2,11 +2,47 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'app',
   'text!templates/auth/signUpTemplate.html'
-], function($, _, Backbone, signUpTemplate){
+], function($, _, Backbone, app, signUpTemplate){
 
   var SignUpView = Backbone.View.extend({
     el: $("#page"),
+
+    initialize: function() {
+      _.bindAll(this);
+
+      // Listen for session logged_in state changes and re-render
+      app.session.on("change:logged_in", this.render);
+    },
+
+    events: {
+      'click #sign-up-btn'            : 'onLoginAttempt'
+    },
+
+    onSignupAttempt: function(event) {
+      if(evt) evt.preventDefault();
+
+      if(this.$("#login-form").parsley('validate')) {
+          app.session.login({
+              username: this.$("#login-username-input").val(),
+              password: this.$("#login-password-input").val()
+          }, {
+              success: function(mod, res){
+                  if(DEBUG) console.log("SUCCESS", mod, res);
+
+              },
+              error: function(err){
+                  if(DEBUG) console.log("ERROR", err);
+                  app.showAlert('Bummer dude!', err.error, 'alert-danger');
+              }
+          });
+      } else {
+          // Invalid clientside validations thru parsley
+          if(DEBUG) console.log("Did not pass clientside validation");
+
+      }
+    },
 
     render: function() {
       $('.menu a').removeClass('active');
