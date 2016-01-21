7dade014646d28834bddd495f2189b8ad287b84f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-01-21 20:53:00 +1300

    Format

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
+ * |--------------------------------------------------------------------------
+ * | Application Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register all of the routes for an application.
+ * | It's a breeze. Simply tell Laravel the URIs it should respond to
+ * | and give it the controller to call when that URI is requested.
+ * |
+ */
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,22 +24,26 @@ Route::get('/', function () {
 // Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 app('Dingo\Api\Exception\Handler')->register(function (Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException $exception) {
-    return Response::make(['error' => 'Hey, what do you think you are doing!?'], 401);
+    return Response::make([ 
+            'error' => 'Hey, what do you think you are doing!?' 
+    ], 401);
 });
 
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
-  $api->get('auth/', 'Nestor\Http\Controllers\Auth\AuthController@checkLogin');
-  // auth
-  $api->post('auth/login', 'Nestor\Http\Controllers\Auth\AuthController@postLogin');
-
-  // registration
-  $api->post('auth/register', 'Nestor\Http\Controllers\UsersController@create');
-
-  // projects
-	$api->get('projects/', 'Nestor\Http\Controllers\ProjectsController@index');
-	$api->get('projects/{id}', 'Nestor\Http\Controllers\ProjectsController@show');
-
-	$api->get('projects/{projectId}/testsuites/{testSuiteId}', ['middleware' => 'api.auth'], 'Nestor\Http\Controllers\TestSuitesController@show');
+    $api->get('auth/', 'Nestor\Http\Controllers\Auth\AuthController@checkLogin');
+    // auth
+    $api->post('auth/login', 'Nestor\Http\Controllers\Auth\AuthController@postLogin');
+    
+    // registration
+    $api->post('auth/register', 'Nestor\Http\Controllers\UsersController@create');
+    
+    // projects
+    $api->get('projects/', 'Nestor\Http\Controllers\ProjectsController@index');
+    $api->get('projects/{id}', 'Nestor\Http\Controllers\ProjectsController@show');
+    
+    $api->get('projects/{projectId}/testsuites/{testSuiteId}', [ 
+            'middleware' => 'api.auth' 
+    ], 'Nestor\Http\Controllers\TestSuitesController@show');
 });
