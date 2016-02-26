4c02eb9c3b4e00e803209e446d07b83d317e9545
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-02-26 21:21:00 +1300

    Formatting

--- public/js/models/core/SessionModel.js
+++ public/js/models/core/SessionModel.js
@@ -1,81 +1,80 @@
-/**
- * @desc		stores the POST state and response state of authentication for user
- */
+// File: SessionMode.js
+
 define([
-  'underscore',
-  'backbone',
-  'models/core/UserModel',
-  'models/core/BaseModel',
+    'underscore',
+    'backbone',
+    'models/core/UserModel',
+    'models/core/BaseModel',
 ], function(_, Backbone, UserModel, BaseModel) {
 
-  var SessionModel = BaseModel.extend({
+    var SessionModel = BaseModel.extend({
 
-  	defaults: {
-  		loggedIn : false,
-      userId: 0
-  	},
+      	defaults: {
+      		loggedIn : false,
+            userId: 0
+      	},
 
-  	initialize: function (options) {
-      _.bindAll(this, 'updateSessionUser', 'checkAuth', 'postAuth', 'login', 'logout', 'signup');
+      	initialize: function (options) {
+            _.bindAll(this, 'updateSessionUser', 'checkAuth', 'postAuth', 'login', 'logout', 'signup');
 
-      // Singleton user object
-      // Access or listen on this throughout any module with app.session.user
-      this.user = new UserModel({});
-  	},
+            // Singleton user object
+            // Access or listen on this throughout any module with app.session.user
+            this.user = new UserModel({});
+      	},
 
-    url: function() {
-      return 'api/auth';
-    },
+        url: function() {
+            return 'api/auth';
+        },
 
-    // Fxn to update user attributes after receiving API response
-    updateSessionUser: function( userData ){
-        this.user.set(_.pick(userData, _.keys(this.user.defaults)));
-    },
+        // Fxn to update user attributes after receiving API response
+        updateSessionUser: function( userData ){
+            this.user.set(_.pick(userData, _.keys(this.user.defaults)));
+        },
 
-    /*
-     * Check for session from API
-     * The API will parse client cookies using its secret token
-     * and return a user object if authenticated
-     */
-    checkAuth: function(callback, args) {
-        var self = this;
-        this.fetch({
-            success: function(mod, res){
-                if(!res.error && res.user){
-                    self.updateSessionUser(res.user);
-                    self.set({ logged_in : true });
-                    if('success' in callback) callback.success(mod, res);
-                } else {
+        /*
+         * Check for session from API
+         * The API will parse client cookies using its secret token
+         * and return a user object if authenticated
+         */
+        checkAuth: function(callback, args) {
+            var self = this;
+            this.fetch({
+                success: function(mod, res){
+                    if(!res.error && res.user){
+                        self.updateSessionUser(res.user);
+                        self.set({ logged_in : true });
+                        if('success' in callback) callback.success(mod, res);
+                    } else {
+                        self.set({ logged_in : false });
+                        if('error' in callback) callback.error(mod, res);
+                    }
+                }, error:function(mod, res){
                     self.set({ logged_in : false });
                     if('error' in callback) callback.error(mod, res);
                 }
-            }, error:function(mod, res){
-                self.set({ logged_in : false });
-                if('error' in callback) callback.error(mod, res);
-            }
-        }).complete( function(){
-            if('complete' in callback) callback.complete();
-        });
-    },
+            }).complete( function(){
+                if('complete' in callback) callback.complete();
+            });
+        },
 
-    login: function(opts, callback, args){
-        this.postAuth(_.extend(opts, { method: 'login' }), callback);
-    },
+        login: function(opts, callback, args){
+            this.postAuth(_.extend(opts, { method: 'login' }), callback);
+        },
 
-    logout: function(opts, callback, args){
-        this.postAuth(_.extend(opts, { method: 'logout' }), callback);
-    },
+        logout: function(opts, callback, args){
+            this.postAuth(_.extend(opts, { method: 'logout' }), callback);
+        },
 
-    signup: function(opts, callback, args){
-        this.postAuth(_.extend(opts, { method: 'signup' }), callback);
-    }
+        signup: function(opts, callback, args){
+            this.postAuth(_.extend(opts, { method: 'signup' }), callback);
+        }
 
-    // removeAccount: function(opts, callback, args){
-    //     this.postAuth(_.extend(opts, { method: 'remove_account' }), callback);
-    // }
+        // removeAccount: function(opts, callback, args){
+        //     this.postAuth(_.extend(opts, { method: 'remove_account' }), callback);
+        // }
 
-  });
+    });
 
-  return SessionModel;
+    return SessionModel;
 
 });
