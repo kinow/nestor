eca8989d6e7d3fe086005b3733f60b5fe164c9c0
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-13 20:13:39 +1200

    Initial model and views for test cases, based on the test suite ones

---
+++ public/js/models/testcase/TestCaseModel.js
@@ -0,0 +1,39 @@
+define([
+    'underscore',
+    'backbone',
+    'models/core/BaseModel',
+], function(_, Backbone, BaseModel) {
+
+    var TestCaseModel = BaseModel.extend({
+
+        defaults: {
+            id: null,
+            project_id: 1,
+            name: 'No test case name',
+            description: 'No description',
+            url: '#/404'
+        },
+
+        initialize: function(options) {
+            _.bindAll(this, 'url', 'parse');
+        },
+
+        url: function() {
+            var url = '/api/projects/' + this.get('project_id') + '/testsuites';
+            if (this.get('id') != undefined && this.get('id') > 0) {
+                url += '/' + this.get('id');
+            }
+            return url;
+        },
+
+        parse: function(obj) {
+            if (typeof(obj.test_suite) != 'undefined')
+                return obj.test_suite;
+            return obj;
+        }
+
+    });
+
+    return TestCaseModel;
+
+});
---
+++ public/js/views/testcases/NewTestCaseView.js
@@ -0,0 +1,83 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'app',
+    'simplemde',
+    'models/testcase/TestSuiteModel',
+    'collections/testsuite/TestSuitesCollection',
+    'text!templates/testsuites/newTestSuiteTemplate.html'
+], function($, _, Backbone, app, SimpleMDE, TestSuiteModel, TestSuitesCollection, newTestSuiteTemplate) {
+
+    var NewTestCaseView = Backbone.View.extend({
+
+        events: {
+            'click #new-testcase-btn': 'save'
+        },
+
+        initialize: function() {
+            _.bindAll(this, 'render', 'save');
+            this.collection = new TestSuitesCollection();
+        },
+
+        render: function(options) {
+            this.parentId = options.parent_id; // FIXME: remove this comment when we prevent insecure object direct reference
+            this.projectId = options.project_id;
+            var compiledTemplate = _.template(newTestSuiteTemplate, {});
+            this.$el.html(compiledTemplate);
+            this.simplemde = new SimpleMDE({
+                autoDownloadFontAwesome: true, 
+                autofocus: false,
+                autosave: {
+                    enabled: false
+                },
+                element: this.$('#testsuite-description-input')[0],
+                indentWithTabs: false,
+                spellChecker: false,
+                tabSize: 4
+            });
+        },
+
+        save: function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (this.$("#new-testsuite-form").parsley().validate()) {
+                this.collection.create({
+                    name: this.$("#testsuite-name-input").val(),
+                    description: this.simplemde.value(),
+                    parent_id: this.parentId,
+                    project_id: this.projectId,
+                    created_by: app.session.user_id
+                }, {
+                    wait: true,
+                    success: function(mod, res) {
+                        app.showAlert('Success!', 'New test suite ' + this.$("#testsuite-name-input").val() + ' created!', 'success')
+                        Backbone.trigger('nestor:navigationtree_changed');
+                        Backbone.history.history.back();
+                    },
+                    error: function(model, response, options) {
+                        var message = _.has(response, 'statusText') ? response.statusText : 'Unknown error!';
+                        if (
+                            _.has(response, 'responseJSON') &&
+                            _.has(response.responseJSON, 'name') &&
+                            _.has(response.responseJSON.name, 'length') &&
+                            response.responseJSON.name.length > 0
+                        ) {
+                            message = response.responseJSON.name[0];
+                        }
+                        app.showAlert('Failed to add new Test Suite', message, 'error');
+                    }
+                });
+            } else {
+                if (typeof DEBUG != 'undefined' && DEBUG) console.log("Did not pass clientside validation");
+            }
+
+            return false;
+        }
+
+    });
+
+    return NewTestCaseView;
+
+});
