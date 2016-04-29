7f627efde235fd5d438a55e01e4bd63ced6b5117
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-29 21:20:55 +1200

    Format nit

--- public/js/views/projects/ProjectsView.js
+++ public/js/views/projects/ProjectsView.js
@@ -1,37 +1,39 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'models/project/ProjectModel',
-  'collections/project/ProjectsCollection',
-  'views/projects/ProjectsListView',
-  'text!templates/projects/projectsTemplate.html'
-], function($, _, Backbone, ProjectModel, ProjectsCollection, ProjectsListView, projectsTemplate){
+    'jquery',
+    'underscore',
+    'backbone',
+    'models/project/ProjectModel',
+    'collections/project/ProjectsCollection',
+    'views/projects/ProjectsListView',
+    'text!templates/projects/projectsTemplate.html'
+], function($, _, Backbone, ProjectModel, ProjectsCollection, ProjectsListView, projectsTemplate) {
 
-  var ProjectsView = Backbone.View.extend({
-    el: $("#page"),
+    var ProjectsView = Backbone.View.extend({
+        el: $("#page"),
 
-    initialize: function(options) {
-        this.page = 1;
-    },
+        initialize: function(options) {
+            this.page = 1;
+        },
 
-    setPage: function(page) {
-        this.page = page;
-    },
+        setPage: function(page) {
+            this.page = page;
+        },
 
-    render: function() {
-        $('.item').removeClass('active');
-        $('.item a[href="#/projects"]').parent().addClass('active');
+        render: function() {
+            $('.item').removeClass('active');
+            $('.item a[href="#/projects"]').parent().addClass('active');
 
-        this.$el.html(projectsTemplate);
-        var projectsCollection = new ProjectsCollection();
-        projectsCollection.setPage(this.page);
-        var projectsListView = new ProjectsListView({collection: projectsCollection}); 
-        // FIXME: garbage collect this subview
-    }
+            this.$el.html(projectsTemplate);
+            var projectsCollection = new ProjectsCollection();
+            projectsCollection.setPage(this.page);
+            var projectsListView = new ProjectsListView({
+                collection: projectsCollection
+            });
+            // FIXME: garbage collect this subview
+        }
 
-  });
+    });
 
-  return ProjectsView;
-  
+    return ProjectsView;
+
 });
