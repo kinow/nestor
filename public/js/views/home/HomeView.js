2aac3767b31144116170bd570cae384c70da323c
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-04-29 21:15:41 +1200

    Format nit

--- public/js/views/home/HomeView.js
+++ public/js/views/home/HomeView.js
@@ -1,24 +1,24 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'text!templates/home/homeTemplate.html'
-], function($, _, Backbone, homeTemplate){
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/home/homeTemplate.html'
+], function($, _, Backbone, homeTemplate) {
 
-  var HomeView = Backbone.View.extend({
-    el: $("#page"),
+    var HomeView = Backbone.View.extend({
+        el: $("#page"),
 
-    initialize: function () {
-        _.bindAll(this, 'render');
-    },
+        initialize: function() {
+            _.bindAll(this, 'render');
+        },
 
-    render: function(){
-      $('.item').removeClass('active');
-      $('.item a[href="#"]').parent().addClass('active');
-      this.$el.html(homeTemplate);
-    }
-  });
+        render: function() {
+            $('.item').removeClass('active');
+            $('.item a[href="#"]').parent().addClass('active');
+            this.$el.html(homeTemplate);
+        }
+    });
 
-  return HomeView;
+    return HomeView;
 
 });
