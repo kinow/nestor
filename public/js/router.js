c13e6442e827f2e51683b215cdb2f7cb1ae57744
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-05-02 21:38:11 +1200

    Add router to show confirm delete test case screen

--- public/js/router.js
+++ public/js/router.js
@@ -491,6 +491,24 @@ define([
             }
         });
 
+        testCasesRouter.on('route:showConfirmDeleteTestCase', function(projectId, testSuiteId, testCaseId) {
+            if (!app.viewProjectView) {
+                app.viewProjectView = new ViewProjectView();
+            }
+
+            app.viewProjectView.setProjectId(projectId);
+            app.viewProjectView.setTestSuiteId(testSuiteId);
+            app.viewProjectView.setTestCaseId(testCaseId);
+            if (typeof app.currentView !== 'undefined' && app.currentView.cid == app.viewProjectView.cid) {
+                app.viewProjectView.displayConfirmDeleteTestCase();
+            } else {
+                app.showView(app.viewProjectView, {
+                    requiresAuth: true,
+                    onSuccess: app.viewProjectView.displayConfirmDeleteTestCase
+                });
+            }
+        });
+
         // --- end test cases router ---
 
         navigation.appendRouter(baseRouter);
