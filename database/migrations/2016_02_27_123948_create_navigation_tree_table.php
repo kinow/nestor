253dc19cd711cca912d21eba7749ec5f5cb151e0
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-06-22 22:07:28 +1200

    Issue #99 add attributes column to nodes

--- database/migrations/2016_02_27_123948_create_navigation_tree_table.php
+++ database/migrations/2016_02_27_123948_create_navigation_tree_table.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+
 class CreateNavigationTreeTable extends Migration
 {
     /**
@@ -18,10 +19,11 @@ class CreateNavigationTreeTable extends Migration
             $table->integer('node_type_id');
             $table->foreign('node_type_id')->references('id')->on('navigation_tree_node_types');
             $table->string('display_name', 255);
+            $table->text('attributes')->nullable();
             $table->timestamps();
             $table->primary(array (
                     'ancestor',
-                    'descendant' 
+                    'descendant'
             ));
         });
     }
