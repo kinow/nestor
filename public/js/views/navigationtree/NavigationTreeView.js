17d46cd84033b1b60e1cdad16a9b968d48efc6dd
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-06-24 22:32:02 +1200

    Move extensions to an external var

--- public/js/views/navigationtree/NavigationTreeView.js
+++ public/js/views/navigationtree/NavigationTreeView.js
@@ -113,139 +113,142 @@ define([
                     self.convertToTree(node, model.children);
                     tree.push(node);
 
+                    var extensions = [];
                     // enable drag and drop
                     if (self.draggable) {
-                        el.fancytree('destroy');
-                        el.fancytree({
-                            source: tree,
-                            extensions: ["dnd"],
-                            activeVisible: true, // Make sure, active nodes are visible (expanded).
-                            aria: false, // Enable WAI-ARIA support.
-                            autoActivate: true, // Automatically activate a node when it is focused (using keys).
-                            autoCollapse: false, // Automatically collapse all siblings, when a node is expanded.
-                            autoScroll: false, // Automatically scroll nodes into visible area.
-                            clickFolderMode: 1, // 1:activate, 2:expand, 3:activate and expand, 4:activate (dblclick expands)
-                            checkbox: false, // Show checkboxes.
-                            debugLevel: 1, // 0:quiet, 1:normal, 2:debug
-                            disabled: false, // Disable control
-                            generateIds: true, // Generate id attributes like <span id='fancytree-id-KEY'>
-                            idPrefix: "ft_", // Used to generate node id´s like <span id='fancytree-id-<key>'>.
-                            icon: true, // Display node icons.
-                            keyboard: true, // Support keyboard navigation.
-                            keyPathSeparator: "/", // Used by node.getKeyPath() and tree.loadKeyPath().
-                            minExpandLevel: 2, // 1: root node is not collapsible
-                            selectMode: 1, // 1:single, 2:multi, 3:multi-hier
-                            tabindex: 0, // Whole tree behaves as one single control
-                            childcounter: {
-                                deep: true,
-                                hideZeros: true,
-                                hideExpanded: true
-                            },
-                            focus: function(e, data) {
-                                var node = data.node;
-                                // Auto-activate focused node after 1 second
-                                if(node.data.href){
-                                    node.scheduleAction("activate", 100000);
+                        extensions.push('dnd');
+                    }
+
+                    el.fancytree('destroy');
+                    el.fancytree({
+                        source: tree,
+                        extensions: extensions,
+                        activeVisible: true, // Make sure, active nodes are visible (expanded).
+                        aria: false, // Enable WAI-ARIA support.
+                        autoActivate: true, // Automatically activate a node when it is focused (using keys).
+                        autoCollapse: false, // Automatically collapse all siblings, when a node is expanded.
+                        autoScroll: false, // Automatically scroll nodes into visible area.
+                        clickFolderMode: 1, // 1:activate, 2:expand, 3:activate and expand, 4:activate (dblclick expands)
+                        checkbox: false, // Show checkboxes.
+                        debugLevel: 1, // 0:quiet, 1:normal, 2:debug
+                        disabled: false, // Disable control
+                        generateIds: true, // Generate id attributes like <span id='fancytree-id-KEY'>
+                        idPrefix: "ft_", // Used to generate node id´s like <span id='fancytree-id-<key>'>.
+                        icon: true, // Display node icons.
+                        keyboard: true, // Support keyboard navigation.
+                        keyPathSeparator: "/", // Used by node.getKeyPath() and tree.loadKeyPath().
+                        minExpandLevel: 2, // 1: root node is not collapsible
+                        selectMode: 1, // 1:single, 2:multi, 3:multi-hier
+                        tabindex: 0, // Whole tree behaves as one single control
+                        childcounter: {
+                            deep: true,
+                            hideZeros: true,
+                            hideExpanded: true
+                        },
+                        focus: function(e, data) {
+                            var node = data.node;
+                            // Auto-activate focused node after 1 second
+                            if(node.data.href){
+                                node.scheduleAction("activate", 100000);
+                            }
+                        },
+                        blur: function(e, data) {
+                            data.node.scheduleAction("cancel");
+                        },
+                        activate: function(e, data) {
+                            if (data.draggable)
+                                return; // prevent false hits
+                            var node = data.node;
+                            if(node.data.href){
+                                Backbone.history.navigate(node.data.href, { trigger: true });
+                            }
+                        },
+                        click: function(e, data) { // allow re-loads
+                            var node = data.node;
+                            if(node.isActive() && node.data.href){
+                                // TODO: data.tree.reactivate();
+                            }
+                        },
+                        dnd: {
+                            preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
+                            preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
+                            autoExpandMS: 400,
+                            dragStart: function(node, data) {
+                                // Defines whether this node is draggable or not
+                                // The top level project should have dnd disabled
+                                if (node.data.node_type_id == 1) { 
+                                    return false;
                                 }
+                                return true;
                             },
-                            blur: function(e, data) {
-                                data.node.scheduleAction("cancel");
+                            dragStop: function(node, data) {
+                                
                             },
-                            activate: function(e, data) {
-                                if (data.draggable)
-                                    return; // prevent false hits
-                                var node = data.node;
-                                if(node.data.href){
-                                    Backbone.history.navigate(node.data.href, { trigger: true });
-                                }
+                            dragOver: function(node, data) {
                             },
-                            click: function(e, data) { // allow re-loads
-                                var node = data.node;
-                                if(node.isActive() && node.data.href){
-                                    // TODO: data.tree.reactivate();
-                                }
-                            },
-                            dnd: {
-                                preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
-                                preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
-                                autoExpandMS: 400,
-                                dragStart: function(node, data) {
-                                    // Defines whether this node is draggable or not
-                                    // The top level project should have dnd disabled
-                                    if (node.data.node_type_id == 1) { 
+                            dragEnter: function(node, data) {
+                                // Defines whether another node can be dragged on this node
+                                // Return ['before', 'after'] to restrict available hitModes.
+                                //  Any other return value will calc the hitMode from the cursor position.
+                                // Prevent dropping a parent below another parent (only sort
+                                // nodes under the same parent)
+                                // if(node.parent !== data.otherNode.parent){
+                                //   return false;
+                                // }
+                                // Don't allow dropping *over* a node (would create a child)
+                                // return ["before", "after"];
+
+                                var selectedNode = data.otherNode; // the selected node
+                                if (selectedNode.data.node_type_id == 3) {
+                                    // Test cases should not be dnd onto projects or test cases, only test suites
+                                    if (node.data.node_type_id != 2) {
                                         return false;
                                     }
-                                    return true;
-                                },
-                                dragStop: function(node, data) {
-                                    
-                                },
-                                dragOver: function(node, data) {
-                                },
-                                dragEnter: function(node, data) {
-                                    // Defines whether another node can be dragged on this node
-                                    // Return ['before', 'after'] to restrict available hitModes.
-                                    //  Any other return value will calc the hitMode from the cursor position.
-                                    // Prevent dropping a parent below another parent (only sort
-                                    // nodes under the same parent)
-                                    // if(node.parent !== data.otherNode.parent){
-                                    //   return false;
-                                    // }
-                                    // Don't allow dropping *over* a node (would create a child)
-                                    // return ["before", "after"];
-
-                                    var selectedNode = data.otherNode; // the selected node
-                                    if (selectedNode.data.node_type_id == 3) {
-                                        // Test cases should not be dnd onto projects or test cases, only test suites
-                                        if (node.data.node_type_id != 2) {
-                                            return false;
-                                        }
-                                        // Test cases should not be dnd onto its own parent
-                                        if (node == selectedNode.parent) {
-                                            return false;
-                                        }
+                                    // Test cases should not be dnd onto its own parent
+                                    if (node == selectedNode.parent) {
+                                        return false;
                                     }
-                                    if (selectedNode.data.node_type_id == 2) {
-                                        // Test suites should not be dnd onto test cases
-                                        if (node.data.node_type_id == 3) {
-                                            return false;
-                                        }
-                                        // Test suites should not be dnd onto its own parent
-                                        if (node == selectedNode.parent) {
-                                            return false;
-                                        }
-                                    }
-                                    return "over";
-                                },
-                                dragDrop: function(node, data) {
-                                    var selectedNode = data.otherNode; // the selected node
-                                    var descendant = '' + selectedNode.data.node_type_id + '-' + selectedNode.data.node_id;
-                                    var ancestor = '' + node.data.node_type_id + '-' + node.data.node_id;
-                                    var url = "/api/navigationtree/move";
-                                    $.ajax({
-                                      type: "POST",
-                                      url: url,
-                                      data: {descendant: descendant, ancestor: ancestor},
-                                      success: function(jqXHR, textStatus, data) {
-                                        selectedNode.moveTo(node, jqXHR.hitMode);
-                                        var rootNode = node.tree.rootNode;
-                                        rootNode.sortChildren(self.sortCmp, true);
-                                        app.showAlert('Success!', 'Moved successfully!', 'success');
-                                      },
-                                      error: function(jqXHR, textStatus, data) {
-                                        var responseText = jQuery.parseJSON(jqXHR.responseText);
-                                        var message = responseText.error.message;
-                                        app.showAlert('Error moving!', message, 'error');
-                                      }
-                                    });
-                                },
-                                dragLeave: function(node, data) {
                                 }
+                                if (selectedNode.data.node_type_id == 2) {
+                                    // Test suites should not be dnd onto test cases
+                                    if (node.data.node_type_id == 3) {
+                                        return false;
+                                    }
+                                    // Test suites should not be dnd onto its own parent
+                                    if (node == selectedNode.parent) {
+                                        return false;
+                                    }
+                                }
+                                return "over";
+                            },
+                            dragDrop: function(node, data) {
+                                var selectedNode = data.otherNode; // the selected node
+                                var descendant = '' + selectedNode.data.node_type_id + '-' + selectedNode.data.node_id;
+                                var ancestor = '' + node.data.node_type_id + '-' + node.data.node_id;
+                                var url = "/api/navigationtree/move";
+                                $.ajax({
+                                  type: "POST",
+                                  url: url,
+                                  data: {descendant: descendant, ancestor: ancestor},
+                                  success: function(jqXHR, textStatus, data) {
+                                    selectedNode.moveTo(node, jqXHR.hitMode);
+                                    var rootNode = node.tree.rootNode;
+                                    rootNode.sortChildren(self.sortCmp, true);
+                                    app.showAlert('Success!', 'Moved successfully!', 'success');
+                                  },
+                                  error: function(jqXHR, textStatus, data) {
+                                    var responseText = jQuery.parseJSON(jqXHR.responseText);
+                                    var message = responseText.error.message;
+                                    app.showAlert('Error moving!', message, 'error');
+                                  }
+                                });
+                            },
+                            dragLeave: function(node, data) {
                             }
-                        });
-                        // end
-                        el.fancytree('getRootNode').sortChildren(self.sortCmp, true);
-                    }
+                        }
+                    });
+                    // end
+                    el.fancytree('getRootNode').sortChildren(self.sortCmp, true);
                 },
                 error: function(collection, response, options) {
                     throw new Error("Failed to fetch projects");
