f794b60f6d32b4d3205b23e3ebeef355906ea6bb
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-02 17:14:24 +1200

    Use prod env in prod, and .env.docker as base. Also call migrations and seed

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,7 +5,11 @@
     git fetch --all
     git reset --hard origin/master
     git pull origin master
+    cp .env.docker .env
+    sed -i 's/=local/=prod/' .env
+    touch database/database.sqlite
     composer dump-autoload
     composer update --no-dev --prefer-dist --profile -vvv --no-scripts
-    php artisan migrate --force
+    php artisan migrate:refresh --force
+    php artisan db:seed
 @endtask
