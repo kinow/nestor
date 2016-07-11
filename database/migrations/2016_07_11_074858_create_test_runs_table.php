4b36c28f582295cd035b66fc9ed2837bd5979c9b
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-11 19:52:16 +1200

    Migration to create test_runs table

---
+++ database/migrations/2016_07_11_074858_create_test_runs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestRunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_runs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('test_plan_id');
+            $table->foreign('test_plan_id')
+                ->references('id')
+                ->on('test_plans')
+                ->onDelete('cascade');
+            $table->string('name', 50);
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('test_runs');
+    }
+}
