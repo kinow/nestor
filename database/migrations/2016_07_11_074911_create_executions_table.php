09c2d7181232236f7c1083fb36dae0009dcd3805
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-11 19:53:42 +1200

    Migration to create executions table

---
+++ database/migrations/2016_07_11_074911_create_executions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExecutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('executions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('test_run_id');
+            $table->foreign('test_run_id')
+                ->references('id')
+                ->on('test_runs')
+                ->onDelete('cascade');
+            $table->integer('test_case_version_id');
+            $table->foreign('test_case_version_id')
+                ->references('id')
+                ->on('test_cases_versions')
+                ->onDelete('cascade');
+            $table->integer('execution_status_id')->default(1);
+            $table->foreign('execution_status_id')
+                ->references('id')
+                ->on('execution_statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('executions');
+    }
+}
