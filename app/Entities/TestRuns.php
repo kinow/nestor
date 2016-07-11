bb3e39cca21f9da79ea40e8e53763fa891e21482
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-11 19:59:51 +1200

    TestRuns entity

---
+++ app/Entities/TestRuns.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Bruno P. Kinoshita, Peter Florijn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Nestor\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class TestRuns extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = ['test_plan_id', 'name', 'description'];
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'test_runs';
+
+    public function testPlan()
+    {
+        return $this->belongsTo('Nestor\\Entities\\TestPlans', 'test_plan_id');
+    }
+}
