66eb03bf65dfe5d8021916285f5a6d2f964a64d6
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-14 19:25:22 +1200

    Format nit

--- public/js/views/testruns/NewTestRunView.js
+++ public/js/views/testruns/NewTestRunView.js
@@ -6,80 +6,80 @@ define([
     'simplemde',
     'collections/testruns/TestRunsCollection',
     'text!templates/testruns/newTestRunTemplate.html'
-], function($, _, Backbone, app, SimpleMDE, TestRunsCollection, newTestRunTemplate) {
+    ], function($, _, Backbone, app, SimpleMDE, TestRunsCollection, newTestRunTemplate) {
 
-    var NewTestRunView = Backbone.View.extend({
-        el: $("#page"),
+        var NewTestRunView = Backbone.View.extend({
+            el: $("#page"),
 
-        initialize: function(options) {
-            _.bindAll(this, 'render', 'save', 'setTestPlanId');
-            this.projectId = 0;
-            this.collection = new TestRunsCollection({ test_plan_id: options.test_plan_id });
-        },
+            initialize: function(options) {
+                _.bindAll(this, 'render', 'save', 'setTestPlanId');
+                this.projectId = 0;
+                this.collection = new TestRunsCollection({ test_plan_id: options.test_plan_id });
+            },
 
-        events: {
-            'click #new-testrun-btn': 'save'
-        },
+            events: {
+                'click #new-testrun-btn': 'save'
+            },
 
-        render: function() {
-            $('.item').removeClass('active');
-            $('.item a[href="#/execution"]').parent().addClass('active');
+            render: function() {
+                $('.item').removeClass('active');
+                $('.item a[href="#/execution"]').parent().addClass('active');
 
-            this.$el.html(newTestRunTemplate);
-            this.simplemde = new SimpleMDE({
-                autoDownloadFontAwesome: true, 
-                autofocus: false,
-                autosave: {
-                    enabled: false
-                },
-                element: $('#testrun-description-input')[0],
-                indentWithTabs: false,
-                spellChecker: false,
-                tabSize: 4
-            });
-        },
-
-        setTestPlanId: function(testPlanId) {
-            this.testPlanId = testPlanId;
-        },
-
-        save: function(event) {
-            var self = this;
-            if (this.$("#new-testrun-form").parsley().validate()) {
-                var testRun = this.collection.create({
-                    name: this.$("#testrun-name-input").val(),
-                    description: this.simplemde.value(),
-                    test_plan_id: self.testPlanId
-                }, {
-                    wait: true,
-                    success: function(mod, res) {
-                        var changedAttributes = testRun.changedAttributes();
-                        var testRunId = changedAttributes.test_run.id;
-                        app.showAlert('Success!', 'New test run ' + this.$("#testrun-name-input").val() + ' created!', 'success')
-                        Backbone.history.navigate("#/execution/" + self.testPlanId + '/testruns/' + testRunId, {
-                            trigger: false
-                        });
+                this.$el.html(newTestRunTemplate);
+                this.simplemde = new SimpleMDE({
+                    autoDownloadFontAwesome: true, 
+                    autofocus: false,
+                    autosave: {
+                        enabled: false
                     },
-                    error: function(model, response, options) {
-                        var message = _.has(response, 'statusText') ? response.statusText : 'Unknown error!';
-                        if (
-                            _.has(response, 'responseJSON') &&
-                            _.has(response.responseJSON, 'name') &&
-                            _.has(response.responseJSON.name, 'length') &&
-                            response.responseJSON.name.length > 0
-                        ) {
-                            message = response.responseJSON.name[0];
+                    element: $('#testrun-description-input')[0],
+                    indentWithTabs: false,
+                    spellChecker: false,
+                    tabSize: 4
+                });
+            },
+
+            setTestPlanId: function(testPlanId) {
+                this.testPlanId = testPlanId;
+            },
+
+            save: function(event) {
+                var self = this;
+                if (this.$("#new-testrun-form").parsley().validate()) {
+                    var testRun = this.collection.create({
+                        name: this.$("#testrun-name-input").val(),
+                        description: this.simplemde.value(),
+                        test_plan_id: self.testPlanId
+                    }, {
+                        wait: true,
+                        success: function(mod, res) {
+                            var changedAttributes = testRun.changedAttributes();
+                            var testRunId = changedAttributes.test_run.id;
+                            app.showAlert('Success!', 'New test run ' + this.$("#testrun-name-input").val() + ' created!', 'success')
+                            Backbone.history.navigate("#/execution/" + self.testPlanId + '/testruns/' + testRunId, {
+                                trigger: false
+                            });
+                        },
+                        error: function(model, response, options) {
+                            var message = _.has(response, 'statusText') ? response.statusText : 'Unknown error!';
+                            if (
+                                _.has(response, 'responseJSON') &&
+                                _.has(response.responseJSON, 'name') &&
+                                _.has(response.responseJSON.name, 'length') &&
+                                response.responseJSON.name.length > 0
+                                ) {
+                                message = response.responseJSON.name[0];
                         }
                         app.showAlert('Failed to add new Test Plan', message, 'error');
                     }
                 });
-            } else {
-                if (typeof DEBUG != 'undefined' && DEBUG) console.log("Did not pass clientside validation");
+                } else {
+                    if (typeof DEBUG != 'undefined' && DEBUG) console.log("Did not pass clientside validation");
+                }
             }
-        }
+
+        });
+
+        return NewTestRunView;
 
     });
-
-    return NewTestRunView;
-
-});
