45c1c01c10fa65d72c72cb8abf51b24c93711b1b
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-08-06 08:13:55 +1200

    Allow user to fill notes

--- app/Entities/Executions.php
+++ app/Entities/Executions.php
@@ -10,7 +10,7 @@ class Executions extends Model implements Transformable
 {
     use TransformableTrait;
 
-    protected $fillable = ['test_run_id', 'test_case_version_id', 'execution_status_id'];
+    protected $fillable = ['test_run_id', 'test_case_version_id', 'execution_status_id', 'notes'];
 
     /**
      * The database table used by the model.
