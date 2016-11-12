da6d6c48548060ac917a40f6598784dfaea4137b
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-11-13 01:37:42 +1300

    Issue #116 adding a command to generate the API documentation

---
+++ app/Console/Commands/GenerateApiDocs.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Nestor\Console\Commands;
+
+use Artisan;
+
+use Illuminate\Console\Command;
+
+class GenerateApiDocs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nestorqa:apidocs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Nestor-QA: generate API documentation';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Generates API documentation. Adds header for jekyll page template.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $exitCode = Artisan::call('api:docs', [
+            '--name' => "Nestor-QA RESTful API", '--use-version' => "v1", '--output-file' => "./docs/documentation/api/api.md"
+        ]);
+
+        if (1 !== $exitCode) {
+            return $exitCode;
+        }
+
+        $args = [];
+        array_push($args, 'sed');
+        array_push($args, '-i');
+        array_push($args, '1i"----\nlayout: page\ntitle: API Installation\n----\n\n"');
+        array_push($args, "./docs/documentation/api/api.md");
+
+        $command = implode(' ', $args);
+        $proc = proc_open($command, [STDIN, STDOUT, STDERR], $pipes);
+        if ($proc === false) {
+            $this->error("Failed to open process");
+            return 2;
+        }
+        return proc_close($proc);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Nestor\Console\Commands\Inspire::class,
+        \Nestor\Console\Commands\GenerateApiDocs::class,
     ];
 
     /**
